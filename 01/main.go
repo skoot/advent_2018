ccbbe08f634f0a9b77f0ea5b73f926ea1808ba80
Olivier Roux <olivier.roux@schibsted.com>
2018-12-18 16:51:40 +0100

    Day 1

--- 01/main.go
+++ 01/main.go
@@ -1,1 +1,77 @@
-package _1
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := readLines("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Phase 1:", total(input))
+	fmt.Println("Phase 2:", findDuplicate(input))
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = file.Close() }()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
+}
+
+func total(changes []string) int {
+	f := 0
+	for _, change := range changes {
+		f += read(change)
+	}
+
+	return f
+}
+
+func findDuplicate(changes []string) int {
+	f := 0
+	found := map[int]bool{f: true}
+
+	i := 0
+	for {
+		f += read(changes[i])
+		if found[f] {
+			return f
+		}
+		found[f] = true
+		if i == len(changes)-1 {
+			i = 0
+		} else {
+			i++
+		}
+	}
+}
+
+func read(s string) int {
+	if strings.HasPrefix(s, "+") {
+		s = s[1:]
+	}
+
+	value, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+
+	return value
+}
--- 01/main_test.go
+++ 01/main_test.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_total(t *testing.T) {
+	input := []string{"+1", "-2", "+3", "+1"}
+	f, err := total(input)
+	require.NoError(t, err)
+	assert.Equal(t, 3, f)
+}
+
+func Test_findDuplicate(t *testing.T) {
+	tests := []struct {
+		name    string
+		changes []string
+		want    int
+	}{
+		{
+			name:    "simple",
+			changes: []string{"+1", "-1"},
+			want:    0,
+		},
+		{
+			name:    "10",
+			changes: []string{"+3", "+3", "+4", "-2", "-4"},
+			want:    10,
+		},
+		{
+			name:    "5",
+			changes: []string{"-6", "+3", "+8", "+5", "-6"},
+			want:    5,
+		},
+		{
+			name:    "14",
+			changes: []string{"+7", "+7", "-2", "-7", "-4"},
+			want:    14,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := findDuplicate(tt.changes)
+			require.NoError(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
